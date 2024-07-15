327ad3e7f184b118e66c8e190a4929c86b2ca1b9
McGarnikle <mcgarnikle@gmail.com>
2024-07-15 19:42:59 -0400

    plus

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = "static-cache-v1";
 const FILES_TO_CACHE = [
 	'offline.html',
 	'index.html',
-	
+
 ];
 
 
@@ -21,11 +21,25 @@ self.addEventListener("install", evt => {
 	self.skipWaiting();
 });
 
+
 self.addEventListener("activate", evt => {
 	console.log("[ServiceWorker] Activate");
 	//Remove previous cached data from disk.
+	evt.waitUntil(
+		caches.keys().then(keyList => {
+			return Promise.all(
+				keyList.map(key => {
+					if (key !== CACHE_NAME) {
+						console.log("[ServiceWorker] Removing old cache", key);
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
 	self.clients.claim();
 });
+
 
 self.addEventListener("fetch", evt => {
 	console.log("[ServiceWorker] Fetch", evt.request.url);
