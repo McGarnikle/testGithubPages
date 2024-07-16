2c0baa875a52de39e4f4c66dd554f061fcc96cfb
McGarnikle <mcgarnikle@gmail.com>
2024-07-15 20:17:22 -0400

    plus

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 //Update cache names any time any of the cached files change.
 // !N’oubliez pas de changer le nom de version du service worker
-const CACHE_NAME = "static-cache-v3";
+const CACHE_NAME = "static-cache-v4";
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
